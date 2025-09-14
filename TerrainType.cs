04dc88c4a2de38805da6668fc2ec31dac4f3d555
Dan S <danieldsachs@gmail.com>
2025-09-14 14:32:39 -0400

    Create TerrainType.cs

---
+++ TerrainType.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace MiningGame
+{
+    /// <summary>
+    /// Defines the three terrain types used in the mining game
+    /// </summary>
+    public enum TerrainType
+    {
+        Empty = 0,      // Can walk through
+        Diggable = 1,   // Can be dug to become empty
+        Undiggable = 2  // Cannot be modified (walls/barriers)
+    }
+}
